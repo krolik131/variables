27e82d3034a2f660a337c9199637d9e775b27bf4
krolik131 <kostya-leteha@mail.ru>
2024-09-05 14:33:02 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,6 @@
+Number_of_completed_homework = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_task = Number_of_hours_spent/Number_of_completed_homework
+print('Курс:',Course_name,',всего задач:',Number_of_completed_homework, ',затрачено часов:', Number_of_hours_spent, ',среднее время выполнения', Time_for_one_task, 'часа.')
+#Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
